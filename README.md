380a597d7458f0bfad30b8edc55b0a79a76fb3ca
1093661893 <1093661893@qq.com>
2024-09-10 02:45:29 +0000

    1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## 项目基于element-plus + vuepress
+## node version > 18.19.0
+> vuepress[https://www.vuepress.cn/]
 > css3 css3的html文件 
 > src 示例demo
 > packages 组件
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import elementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'element-plus/dist/index.css'
+import * as ElIcon from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+Object.keys(ElIcon).forEach((key) => {
+  app.component(key, ElIcon[key])
+})
 
 app.use(createPinia())
 app.use(router)
